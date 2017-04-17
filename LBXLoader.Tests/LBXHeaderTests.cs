4ba26a0100ea01b851f2322cc93fd43047c60c6b
George Legeza <glegeza@gmail.com>
2017-04-17 16:33:35 -0400

    Stub out LBXHeader tests

---
+++ LBXLoader.Tests/LBXHeaderTests.cs
@@ -0,0 +1,50 @@
+﻿namespace DLS.LBXLoader.Tests
+{
+    using NUnit.Framework;
+    using System;
+    using System.Text;
+
+    class LBXHeaderTests
+    {
+        [TestFixture]
+        class ConstructorTest
+        {
+            [Test]
+            private void FileCountIsProperyRead()
+            {
+
+            }
+
+            [Test]
+            private void SignatureIsProperlyRead()
+            {
+
+            }
+
+            [Test]
+            private void FileOffsetsProperlyRead()
+            {
+
+            }
+
+            [Test]
+            private void InvalidSignatureThrows()
+            {
+
+            }
+
+            [Test]
+            private void ArrayTooShortThrows()
+            {
+
+            }
+
+            private Byte[] BuildTestHeader(uint totalSize, UInt32[] fileOffsets)
+            {
+                var array = new byte[totalSize];
+
+                return array;
+            }
+        }
+    }
+}
