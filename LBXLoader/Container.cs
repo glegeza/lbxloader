37c126840385544aec8f4b9c78d164481836e708
George Legeza <glegeza@gmail.com>
2017-04-17 22:30:08 -0400

    Basic implementations of PackedFile and Container

--- LBXLoader/Container.cs
+++ LBXLoader/Container.cs
@@ -2,10 +2,6 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Linq;
 
     public class Container
     {
@@ -16,7 +12,32 @@
         /// </param>
         public Container(Byte[] file)
         {
+            var header = new Header(file);
+            var fileDescs = new List<FileDescription>();
 
+            var fileDescRange = header.FileOffsets[0] - 512;
+            for (var i = 512; i < header.FileOffsets[0]; i += 32)
+            {
+                var desc = new FileDescription(file, i);
+                fileDescs.Add(desc);
+            }
+
+            var packedFiles = new List<PackedFile>();
+            for (var i = 0; i < header.FileOffsets.Length - 1; i++)
+            {
+                var size = header.FileOffsets[i + 1] - header.FileOffsets[i];
+                FileDescription desc = null;
+                if (i < fileDescs.Count)
+                {
+                    desc = fileDescs[i];
+                }
+                var packedFile = new PackedFile(file, header.FileOffsets[i], size, desc);
+                packedFiles.Add(packedFile);
+            }
+
+            Files = packedFiles;
         }
+
+        public IEnumerable<PackedFile> Files { get; private set; }
     }
 }
---
+++ LBXLoader/PackedFile.cs
@@ -0,0 +1,17 @@
+﻿namespace DLS.LBXLoader
+{
+    using System;
+
+    public class PackedFile
+    {
+        public FileDescription Description { get; private set; }
+        public byte[] FileBytes { get; private set; }
+
+        public PackedFile(byte[] file, uint offset, uint size, FileDescription desc)
+        {
+            FileBytes = new byte[size];
+            Array.Copy(file, offset, FileBytes, 0, size);
+            Description = desc;
+        }
+    }
+}
